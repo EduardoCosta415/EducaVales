838e0910f0a94dbbab8ce792df734007d444447a
EduardoCosta415 <eduardocosta4155@gmail.com>
2025-09-08 14:43:20 -0300

    Atualização EducaVales

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import dotenv from "dotenv";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 export default defineConfig({
   plugins: [
     react(),
-    runtimeErrorOverlay(),
+    
     ...(process.env.NODE_ENV !== "production" &&
     process.env.REPL_ID !== undefined
       ? [
